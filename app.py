5ced63329033842f846714ad3c8b3d7042397be0
Ubuntu <ubuntu@ip-10-202-102-192.ec2.internal>
2013-02-08 23:37:24 +0000

    Cleanup

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from flask_peewee.db import MySQLDatabase
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
 
-#db = MySQLDatabase(app.config['DATABASE'])
-db = MySQLDatabase('rafarafi_wheels', **{'passwd': 'ktd#433', 'host': 'pragmaticapi.com', 'user': 'rafarafi_admin'})
+db = MySQLDatabase('wheels', **app.config['DATABASE'])
 
 @app.before_request
 def before_request():
